a1c3bb12c4e2f65d081fa53b2662253f92bd20c2
ingridlyra <ingridmlbl@gmail.com>
2021-03-31 19:05:08 -0300

    first commit

---
+++ src/main/java/com/mkMagic/makeMagic/MakeMagicApplication.java
@@ -0,0 +1,13 @@
+package com.mkMagic.makeMagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMagicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkMagic/makeMagic/MakeMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkMagic.makeMagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
