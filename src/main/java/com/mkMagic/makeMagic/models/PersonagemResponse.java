152f4a734c0fb6237b013a6b9aff0c4e03243053
ingridlyra <ingridmlbl@gmail.com>
2021-04-01 22:54:07 -0300

    Defined the PersonagemResponse class

--- src/main/java/com/mkMagic/makeMagic/models/PersonagemResponse.java
+++ src/main/java/com/mkMagic/makeMagic/models/PersonagemResponse.java
@@ -1,4 +1,57 @@
 package com.mkMagic.makeMagic.models;
 
 public class PersonagemResponse {
+    private String name;
+    private String role;
+    private String school;
+    private String house;
+    private String patronus;
+
+    public PersonagemResponse(Personagem personagem) {
+        this.name = personagem.getName();
+        this.role = personagem.getRole();
+        this.school = personagem.getSchool();
+        this.house = personagem.getHouse();
+        this.patronus = personagem.getPatronus();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getPatronus() {
+        return patronus;
+    }
+
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
 }
