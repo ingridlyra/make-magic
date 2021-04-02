0dcb13df44463ba445ed030454572560c60fdbb9
ingridlyra <ingridmlbl@gmail.com>
2021-04-02 20:28:42 -0300

    Defined the IdNotFoundException

--- src/main/java/com/mkMagic/makeMagic/models/exceptions/IdNotFoundException.java
+++ src/main/java/com/mkMagic/makeMagic/models/exceptions/IdNotFoundException.java
@@ -1,4 +1,12 @@
 package com.mkMagic.makeMagic.models.exceptions;
 
-public class IdNotFoundException {
+public class IdNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public IdNotFoundException() {
+    }
+
+    public IdNotFoundException(String message) {
+        super(message);
+    }
 }
